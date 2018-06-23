033172e17c03dcc39cb9e07982cbd64f2256d9d8
Andrea <ellisona@gmail.com>
2018-06-22 22:31:09 -0700

    Made far too many changes for one commit... ensured that bin files are correct.  Having issues with postgres integration and will have to investigate those scripts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407214546) do
+ActiveRecord::Schema.define() do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
